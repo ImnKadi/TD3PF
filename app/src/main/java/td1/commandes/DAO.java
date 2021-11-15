011d0e9c6d7750b87f2a5ade278534ee4066111f
Imane Kadi <imanemnds@gmail.com>
2021-11-15 16:53:08 +0100

    Debut exo1 qst1

---
+++ app/src/main/java/td1/commandes/DAO.java
@@ -0,0 +1,97 @@
+package td1.commandes;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+
+import static td1.commandes.Categorie.*;
+import td1.paires.Paire;
+
+public class DAO {
+    private List<Commande> commandes;
+    private DAO(Commande c1, Commande ...cs) {
+        commandes = new ArrayList<>();
+        commandes.add(c1);
+        commandes.addAll(List.of(cs));
+    }
+
+    public static DAO instance = null;
+
+    public static final DAO instance() {
+        if (instance == null) {
+            Produit camembert = new Produit("Camembert", 4.0, NORMAL);
+            Produit yaourts = new Produit("Yaourts", 2.5, INTERMEDIAIRE);
+            Produit masques = new Produit("Masques", 25.0, REDUIT);
+            Produit gel = new Produit("Gel", 5.0, REDUIT);
+            Produit tournevis = new Produit("Tournevis", 4.5, NORMAL);
+            //
+            Commande c1 = new Commande()
+                .ajouter(camembert, 1)
+                .ajouter(yaourts, 6);
+            Commande c2 = new Commande()
+                .ajouter(masques, 2)
+                .ajouter(gel, 10)
+                .ajouter(camembert, 2)
+                .ajouter(masques, 3);
+            //
+            instance = new DAO(c1,c2);
+        }
+        return instance;
+    }
+
+    /**
+     * liste de toutes les commandes
+     */
+    public List<Commande> commandes() {
+        return commandes;
+    }
+
+    /**
+     * ensemble des différents produits commandés
+     */
+    public Set<Produit> produits() {
+        Set<Produit> res = new HashSet<>();
+        for(Commande c : commandes){
+            for(Paire<Produit, Integer> p : c.lignes()){
+
+        }
+        return res;
+    }
+
+    /**
+     * liste des commandes vérifiant un prédicat
+     */
+     public List<Commande> selectionCommande(Predicate<Commande> p) {
+
+       }
+
+
+    /**
+     * liste des commandes dont au moins une ligne vérifie un prédicat
+     */
+    public List<Commande> selectionCommandeSurExistanceLigne(Predicate<Paire<Produit,Integer> p) {
+   //  List<Commande> res = new ArrayList<>();
+   //  for(Commande c : commandes){
+     }
+   //  return res;
+    }
+
+    /**
+     * ensemble des différents produits commandés vérifiant un prédicat
+     */
+    public Set<Produit> selectionProduits(Predicate<Produit> p) {
+
+        Set<Produit> res = new HashSet<>();
+        for(Produit x : produits()){
+            if(p.test(x));
+            res.add(x);
+        }
+
+        return res;
+
+    }
+
+}
