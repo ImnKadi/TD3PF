a32c43f4fb19fdfe285bf12a60c570e9dbaff97c
Imane Kadi <imanemnds@gmail.com>
2021-11-22 14:34:31 +0100

    exo 2 méthodes, 2 premières fonctions update

--- app/src/main/java/td2/App.java
+++ app/src/main/java/td2/App.java
@@ -74,7 +74,7 @@ public class App {
 
 
     // matières d'un étudiant
-    public static final Function<Etudiant, Stream<Matiere>> matieresE = e -> e.ues().stream().flatMap(ue -> ue.ects().keySet().stream());
+    public static final Function<Etudiant, Stream<Matiere>> matieresE = e -> matieresA.apply(e.annee());
 
     
 
